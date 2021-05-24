a9a945985fbfb0f39c0dfd4b452665f1936e4129
chris <cseiter@gmail.com>
2021-05-23 20:31:18 -0400

    fixed uppercase/lowercase error, tests passed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Task from "./Task";
 
 function TaskList({tasks,deleteTask}) {
-  const TaskList = tasks.map((task) => (
+  const taskList = tasks.map((task) => (
     <Task
         key={task.text}
         text={task.text}
