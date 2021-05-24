2e35abd5fc33d27b99738ed2cffa49fcf53137de
chris <cseiter@gmail.com>
2021-05-23 20:11:31 -0400

    map tasks import from task comp in tasklist comp

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks,deleteTask}) {
+  const TaskList = tasks.map((task) => (
+    <Task
+        key={task.text}
+        text={task.text}
+        category={task.category}
+        deleteTask={deleteTask}
+        />
+  ));
   return (
     <div className="tasks">
+      {taskList}
       {/* display a list of tasks using Task component */}
     </div>
   );
